83e05af2e3e07e629c33f5d6c1c453f1f7eb2e69
Safut <37475625+Safut@users.noreply.github.com>
2018-03-17 18:45:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const User = require('./models/user');
 const hours_12 = 43200000;
 const port = 3000;
-const url = 'https://ebed0ba9.ngrok.io';
 
 
 const TelegramBot = require('node-telegram-bot-api');
