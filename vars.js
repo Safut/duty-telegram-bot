95de457e88d20d917cc0274fc1ed353115609176
Safut <37475625+Safut@users.noreply.github.com>
2018-04-22 17:11:27 +0500

    Update vars.js

--- vars.js
+++ vars.js
@@ -49,4 +49,4 @@ const dutyList = {
   module.exports.dayShift = dayShift;
   module.exports.nightShift = nightShift;
   module.exports.token = '587010368:AAHLJu72RO8yBW6n1k2LDVon8vpScy0B9LY';
-  module.exports.mongoUrl = 'mongodb://duty:advancE>@ds115579.mlab.com:15579/duty';
+  module.exports.mongoUrl = 'mongodb://duty:advancE@ds115579.mlab.com:15579/duty';
