e4551b99b287241e3ecd67414de3d6286a039264
Safut <37475625+Safut@users.noreply.github.com>
2018-07-18 22:27:38 +0500

    Add files via upload

---
+++ vars.js
@@ -0,0 +1,52 @@
+const dutyList = {
+    Abror:['+998908086628', 'Сапарбаев Аброр'], Albert:['+998935270347', 'Хабибулин Альберт'], 
+    Andrey:['+998983645334', 'Валл Андрей'], Ivan:['+998903450041', 'Ефименко Иван']
+    };
+  
+  const holidayList = {
+    reply_markup: {
+    inline_keyboard: 
+    [
+      [{ text: 'Abror', callback_data: 'holiday Abror' }],
+      [{ text: 'Albert', callback_data: 'holiday Albert' }],
+      [{ text: 'Andrey', callback_data: 'holiday Andrey' }],
+      [{ text: 'Ivan', callback_data: 'holiday Ivan' }],
+      [{ text: 'отмена', callback_data: 'cancel' }]
+    ]
+  }
+  };
+  
+  
+  const dayShift = {
+    reply_markup: {
+    inline_keyboard: 
+    [
+      [{ text: 'Abror', callback_data: 'dayShift Abror' }],
+      [{ text: 'Albert', callback_data: 'dayShift Albert' }],
+      [{ text: 'Andrey', callback_data: 'dayShift Andrey' }],
+      [{ text: 'Ivan', callback_data: 'dayShift Ivan' }],
+      [{ text: 'отмена', callback_data: 'cancel' }]
+    ]
+  }
+  };
+  
+  
+  const nightShift = {
+    reply_markup: {
+    inline_keyboard: 
+    [
+      [{ text: 'Abror', callback_data: 'nightShift Abror' }],
+      [{ text: 'Albert', callback_data: 'nightShift Albert' }],
+      [{ text: 'Andrey', callback_data: 'nightShift Andrey' }],
+      [{ text: 'Ivan', callback_data: 'nightShift Ivan' }],
+      [{ text: 'отмена', callback_data: 'cancel' }]
+    ]
+  }
+  };
+
+  module.exports.dutyList = dutyList;
+  module.exports.holidayList = holidayList;
+  module.exports.dayShift = dayShift;
+  module.exports.nightShift = nightShift;
+  module.exports.token = '587010368:AAHLJu72RO8yBW6n1k2LDVon8vpScy0B9LY';
+  module.exports.mongoUrl = 'mongodb://duty:advancE@ds115579.mlab.com:15579/duty';
