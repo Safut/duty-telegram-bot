fd19fdc8857841bcb15299733976ce03a9b01582
Safut <37475625+Safut@users.noreply.github.com>
2019-01-14 03:17:12 +0500

    Delete vars.js

--- vars.js
+++
@@ -1,7 +0,0 @@
-const token = '534477980:AAGmXdQADf4XLTAYJQrnWcP7JulJNH6H-60',
-    mongoUrl = 'mongodb://duty:advancE@ds115579.mlab.com:15579/duty';
-
-module.exports = {
-    token: token,
-    mongoUrl: mongoUrl
-}
